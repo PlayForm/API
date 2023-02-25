dc38d3bfa1c751b85c915b5de7b40dc4bca31290
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-25 03:39:18 +0200

    squash!

--- dist/lib/env.js
+++ dist/lib/env.js
@@ -1,1 +1,4 @@
-import*as e from"dotenv";import{z as o}from"zod";e.config();var t=o.object({DISCORD_PUBLIC_KEY:o.string().default("")}).parse(process.env);export{t as default};
+import * as e from "dotenv";
+import { z as o } from "zod";
+e.config();var t=o.object({DISCORD_PUBLIC_KEY:o.string().default("")}).parse(process.env);export { t as default };
+
--- dist/options/router.js
+++ dist/options/router.js
@@ -1,1 +1,4 @@
-import"node-fetch";import r from"../lib/env.js";var t=[{preflight:()=>{if(!r.DISCORD_PUBLIC_KEY)throw new Error("The DISCORD_TOKEN environment variable is required.")}}];export{t as default};
+import "node-fetch";
+import r from "../lib/env.js";
+var t=[{preflight:()=>{if(!r.DISCORD_PUBLIC_KEY)throw new Error("The DISCORD_TOKEN environment variable is required.")}}];export { t as default };
+
