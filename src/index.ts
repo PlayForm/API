a179f5ee5b8f3692fd0fcff0e5a23d62c97d77ee
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-30 01:52:34 +0300

    squash!

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import type { ResponseInit } from "@cloudflare/workers-types";
-import { Request, Response } from "@cloudflare/workers-types";
 import {
 	InteractionResponseType,
 	InteractionType,
 	verifyKey,
 } from "discord-interactions";
 import { Router } from "itty-router";
+
+import type { ResponseInit } from "@cloudflare/workers-types";
+import { Request, Response } from "@cloudflare/workers-types";
+
 import env from "./lib/env.js";
 
 class JSONResponse extends Response {
