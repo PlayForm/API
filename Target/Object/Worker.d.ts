fd0192439d4ecab7133bdf6e1a501ee6bdfee7de
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-05 17:53:52 +0300

    squash!

--- Target/Object/Worker.d.ts
+++ Target/Object/Worker.d.ts
@@ -1,31 +1,7 @@
 declare const _default: {
-    fetch(request: Request, { DISCORD_PUBLIC_KEY }?: {
-        DISCORD_APPLICATION_ID: string;
-        DISCORD_CLIENT_ID: string;
-        DISCORD_CLIENT_SECRET: string;
-        DISCORD_PUBLIC_KEY: string;
-        DISCORD_TOKENS: string;
-    }): Promise<any>;
+    fetch(request: Request, { DISCORD_PUBLIC_KEY }?: any): Promise<any>;
 };
 export default _default;
 import type { Request } from "@cloudflare/workers-types";
-export declare const Environment: import("zod").ZodObject<{
-    DISCORD_APPLICATION_ID: import("zod").ZodDefault<import("zod").ZodOptional<import("zod").ZodString>>;
-    DISCORD_CLIENT_ID: import("zod").ZodDefault<import("zod").ZodOptional<import("zod").ZodString>>;
-    DISCORD_CLIENT_SECRET: import("zod").ZodDefault<import("zod").ZodOptional<import("zod").ZodString>>;
-    DISCORD_PUBLIC_KEY: import("zod").ZodDefault<import("zod").ZodOptional<import("zod").ZodString>>;
-    DISCORD_TOKENS: import("zod").ZodDefault<import("zod").ZodOptional<import("zod").ZodString>>;
-}, "strip", import("zod").ZodTypeAny, {
-    DISCORD_APPLICATION_ID: string;
-    DISCORD_CLIENT_ID: string;
-    DISCORD_CLIENT_SECRET: string;
-    DISCORD_PUBLIC_KEY: string;
-    DISCORD_TOKENS: string;
-}, {
-    DISCORD_APPLICATION_ID?: string | undefined;
-    DISCORD_CLIENT_ID?: string | undefined;
-    DISCORD_CLIENT_SECRET?: string | undefined;
-    DISCORD_PUBLIC_KEY?: string | undefined;
-    DISCORD_TOKENS?: string | undefined;
-}>;
+export declare const Environment: any;
 export declare const Response: typeof import("@cloudflare/workers-types").Response;
